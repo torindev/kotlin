9ec07a7b76dd8c9fb79f88027c424e44d43d4807
torindev <torindev3@gmail.com>
2019-02-16 16:22:28 +0200

    lazy commit, always the same

--- KotlinCommonTestDrive/src/Main.kt
+++ KotlinCommonTestDrive/src/Main.kt
@@ -3,6 +3,7 @@ import chapter_1_2_3_4.runDelegationTest
 import chapter_3.runChapter3
 import java.util.*
 
-fun main(args: Array<String>){
-    runChapter3()
+fun main(args: Array<String>) {
+    println(Date(1548923323000))
+    println(Date(1548924590))
 }
