7aa1bc6fa7f513f146e9c8a7e5bf9b985ecf6a91
torindev <torindev3@gmail.com>
2019-03-09 17:10:57 +0200

    lazy commit, always the same

--- KotlinCommonTestDrive/src/chapter_6/TypesAndNullable_6_1.kt
+++ KotlinCommonTestDrive/src/chapter_6/TypesAndNullable_6_1.kt
@@ -1,5 +1,7 @@
 package chapter_6
 
+/* ВОДДЕРЖКА ЗНАЧЕНИЯ NULL и обработка различных ситуаций */
+
 fun run6_1() {
     val lines = arrayOf(
         "str1",
@@ -25,11 +27,87 @@ fun run6_1() {
         println(line?.length?:"empty string")
     }
 
+    //===============================================================================================================
+
     testStringAs("somebody")
     testStringAs(42)
     testStringAs(null)
 
+    //===============================================================================================================
 
+    /* так же есть способ заставить компилятор котлина работать в стиле джавы
+     * для этого нужно после присвоения значения указать !!, что будет означать что тут допускается null
+     * строки ниже вызовут NPE */
+    //val someStringMayNull: String = null!!
+    //someStringMayNull.length
+
+    //===============================================================================================================
+
+    val dangerString: String? = null
+    // не получится, потому что ожидает именно String а не String?
+    //needStringExplicit(dangerString)
+
+    // а вот так можно. let проверяет на null и передает в лямбду сам объект (в данном случае String)
+    // т.е просто превращает String? в String
+    dangerString?.let { str -> needStringExplicit(str) }
+
+    // или если еще больше сократить
+    dangerString?.let { needStringExplicit(it) }
+
+    //===============================================================================================================
+
+    val test11 = MyServiceTest()
+    /* Так получим эксепшнн
+     * kotlin.UninitializedPropertyAccessException: lateinit property someBody has not been initialized
+     * Это благодоря использованию модификатора lateinit, теперь вместо NPE сразу понятно в чем оишбка
+     * к тому же, если не использовать lateinit, Kotlin будет ругаться что поле не проинициализировано */
+    //test11.runService()
+
+    test11.lateInit()
+    test11.runService()
+
+    //===============================================================================================================
+
+    val str1: String? = "sdas"
+    val str2: String? = "fsdfds"
+    val str3: String? = ""
+    val str4: String? = null
+
+    println(str1.isNullOrEmpty())
+    println(str2.isNullOrEmpty())
+    println(str3.isNullOrEmpty())
+    println(str4.isNullOrEmpty())
+
+    println()
+    println(1_000_000)
+
+}
+
+class MyServiceTest {
+
+    private lateinit var someBody: MyService
+
+    fun lateInit() {
+        someBody = MyService("some service")
+    }
+
+    fun runService() {
+        someBody.run()
+    }
+
+
+}
+
+class MyService {
+    constructor(name: String) {
+
+    }
+
+    fun run() {}
+}
+
+fun needStringExplicit(value: String) {
+    println("ta-ta-ta and $value")
 }
 
 //знак ? псоле типа означает что значение может содержать null
