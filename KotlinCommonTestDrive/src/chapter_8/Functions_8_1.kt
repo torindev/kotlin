abba91ffec718b765ea643f337d4a3f61bc7fb2a
torindev <torindev3@gmail.com>
2019-03-16 19:37:05 +0200

    edits

--- KotlinCommonTestDrive/src/chapter_8/Functions_8_1.kt
+++ KotlinCommonTestDrive/src/chapter_8/Functions_8_1.kt
@@ -5,5 +5,17 @@ import common.logg
 fun run8_1() {
     logg("Ф-ции высшего порядка")
     val sum = {x: Int, y: Int -> x + y}
+    // или явное определение
+    val sum2: (Int, Int) -> Int = {x, y -> x + y}
     println(sum(2, 3))
+    println(sum2(2, 3))
+
+    logg("Передача колбека в параметр ф-ции")
+    twoAndThree { a, b -> a + b }
+    twoAndThree { a, b -> a * b }
+}
+
+fun twoAndThree(operation: (Int, Int) -> Int) {
+    val result = operation(2, 3)
+    println("The result is $result")
 }
