221d69f20f08c4636f9f885dd510d4911ca4783d
torindev <torindev3@gmail.com>
2019-03-19 01:20:29 +0200

    edits

--- KotlinCommonTestDrive/src/chapter_8/Functions_8_1.kt
+++ KotlinCommonTestDrive/src/chapter_8/Functions_8_1.kt
@@ -1,6 +1,7 @@
 package chapter_8
 
 import common.logg
+import java.lang.StringBuilder
 
 fun run8_1() {
     logg("Ф-ции высшего порядка")
@@ -13,9 +14,54 @@ fun run8_1() {
     logg("Передача колбека в параметр ф-ции")
     twoAndThree { a, b -> a + b }
     twoAndThree { a, b -> a * b }
+    callbackTest { someValueFromCallbackTest ->
+        run {
+            println(someValueFromCallbackTest)
+        }
+    }
+    println("ab1c".filter { it in 'a'..'z' })
+
+    val letters = listOf("Alpha", "Beta")
+    println(letters.joinToString())
+    println(letters.joinToString {it.toLowerCase()})
+    println(letters.joinToString(
+        separator = "! ",
+        postfix = "! ",
+        transform = { it.toUpperCase() }
+    ))
 }
 
 fun twoAndThree(operation: (Int, Int) -> Int) {
     val result = operation(2, 3)
     println("The result is $result")
+}
+
+fun callbackTest(callback: (Int) -> Unit) {
+    callback(5)
+}
+
+fun String.filter(predicate: (Char) -> Boolean): String {
+    val sb = StringBuilder()
+    for (index in 0 until length) {
+        val element = get(index)
+        if (predicate(element)) sb.append(element)
+    }
+    return sb.toString()
+}
+
+fun <T> Collection<T>.joinToString(
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = "",
+    transform: (T) -> String = { it.toString() }
+): String {
+    val result = StringBuilder()
+
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(transform(element))
+    }
+
+    result.append(postfix)
+    return result.toString()
 }
